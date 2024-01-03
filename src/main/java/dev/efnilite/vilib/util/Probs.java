bbeddc3203bb9915b6eb5ce8d3b9a12dabd461b8
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-01-03 14:45:15 +0100

    Added random instance support to Probs

--- src/main/java/dev/efnilite/vilib/util/Probs.java
+++ src/main/java/dev/efnilite/vilib/util/Probs.java
@@ -5,17 +5,24 @@ import org.jetbrains.annotations.NotNull;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class Probs {
-
-    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     /**
      * @return A random chance with range 0 (inclusive) to 1 (exclusive).
      */
     public static double random() {
-        return random.nextDouble(1);
+        return random(ThreadLocalRandom.current());
+    }
+
+    /**
+     * @param random The random instance.
+     * @return A random chance with range 0 (inclusive) to 1 (exclusive).
+     */
+    public static double random(Random random) {
+        return random.nextDouble();
     }
 
     /**
@@ -32,10 +39,11 @@ public final class Probs {
 
     /**
      * @param distribution A map where each key is mapped to a probability.
+     * @param random       The random instance.
      * @param <K>          The key type.
      * @return A random item from the list, based on the probabilities.
      */
-    public static <K> K random(@NotNull Map<K, Double> distribution) {
+    public static <K> K random(@NotNull Map<K, Double> distribution, Random random) {
         if (distribution.isEmpty()) {
             throw new NoSuchElementException("No elements in distribution map");
         }
@@ -48,12 +56,22 @@ public final class Probs {
             cumulative.put(entry.getKey(), total);
         }
 
-        double random = random() * total;
+        double randomNum = random(random) * total;
         for (Map.Entry<K, Double> entry : cumulative.entrySet()) {
-            if (entry.getValue() >= random) {
+            if (entry.getValue() >= randomNum) {
                 return entry.getKey();
             }
         }
+
         return distribution.keySet().stream().findFirst().orElseThrow();
+    }
+
+    /**
+     * @param distribution A map where each key is mapped to a probability.
+     * @param <K>          The key type.
+     * @return A random item from the list, based on the probabilities.
+     */
+    public static <K> K random(@NotNull Map<K, Double> distribution) {
+        return random(distribution, ThreadLocalRandom.current());
     }
 }
